48644078f57e50193a8c0d24de457f6a8c6dcb95
Erik <enodeg@gmail.com>
2017-01-09 18:53:11 +0200

    Scanner

--- Program.java
+++ Program.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Program
 {
 	public static void main(String[] args)
 	{
 	System.out.println("Hello world");
 	System.out.println("Happy new year!");
+	Scanner scanner = new Scanner(System.in);
 	}
 	}
